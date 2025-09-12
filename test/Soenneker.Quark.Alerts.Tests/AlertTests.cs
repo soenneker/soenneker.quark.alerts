a3a9f664ce2ce012fcd07d439e3a5f70110f1835
Jake Soenneker <jake@soenneker.com>
2025-09-12 15:55:53 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Alerts.Tests/AlertTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.Alerts.Tests;
+
+[Collection("Collection")]
+public sealed class AlertTests : FixturedUnitTest
+{
+    public AlertTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
